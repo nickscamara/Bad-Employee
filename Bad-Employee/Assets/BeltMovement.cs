fbc36a9fbf7132db40ee95921a3132110b0b0f34
Nicolas <nicolascamara29@gmail.com>
2018-11-08 23:52:04 -0500

    final commit day 1

---
+++ Bad-Employee/Assets/BeltMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltMovement : MonoBehaviour {
+
+
+    public int speed = 20;
+    bool move = false;
+    public GameObject obj;
+	
+	
+	// Update is called once per frame
+	void Update () {
+        if (move)
+        {
+            obj.transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("COllision detected");
+        if (col.gameObject.name == "box")
+        {
+            move = true;
+            
+        }
+    }
+}
---
+++ Bad-Employee/Assets/Scripts/Move.cs
@@ -0,0 +1,74 @@
+﻿
+using UnityEngine;
+using System.Collections;
+ 
+ public class Move : MonoBehaviour
+{
+    private const int VECTOR_COUNT = 5;
+    private int vectorIndex = 0;
+    private Vector3[] positionChanges;
+    private Vector3 prevMousePosition;
+    private float[] prevTimes;
+    private bool curDown = false;
+    private bool prevDown = false;
+    public Rigidbody2D rigidbody2D;
+
+    void Start()
+    {
+        prevMousePosition = Vector3.zero;
+        positionChanges = new Vector3[VECTOR_COUNT];
+        prevTimes = new float[VECTOR_COUNT];
+        for (int i = 0; i < VECTOR_COUNT; i++)
+        {
+            positionChanges[i] = Vector3.zero;
+            prevTimes[i] = 0;
+        }
+    }
+
+    void Update()
+    {
+        if (curDown)
+        {
+            // if still down just follow the mouse (finger)
+            Vector3 amountMoved = Camera.main.ScreenToWorldPoint(positionChanges[(vectorIndex - 1 + VECTOR_COUNT) % VECTOR_COUNT]) - Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+            transform.position = transform.position + amountMoved;
+        }
+        else if (prevDown)
+        {
+            // just came up so calculate and set velocity
+            float totalTime = 0;
+            Vector3 totalDistance = Vector3.zero;
+            Vector3 velocity;
+            for (int i = 0; i < VECTOR_COUNT; i++)
+            {
+                totalTime += prevTimes[i];
+                totalDistance += positionChanges[i];
+            }
+            velocity = totalDistance / ( totalTime * 10 );
+            velocity = Camera.main.ScreenToWorldPoint(velocity) - Camera.main.ScreenToWorldPoint(Vector3.zero);
+            rigidbody2D.velocity = velocity;
+        }
+
+        positionChanges[vectorIndex] = Input.mousePosition - prevMousePosition;
+        prevTimes[vectorIndex] = Time.deltaTime;
+        vectorIndex = (vectorIndex + 1) % VECTOR_COUNT;
+
+        prevDown = curDown;
+        prevMousePosition = Input.mousePosition;
+
+       
+    }
+
+    void OnMouseDown()
+    {
+        print("down");
+        curDown = true;
+        rigidbody2D.velocity = Vector2.zero;
+    }
+
+    void OnMouseUp()
+    {
+        print("up");
+        curDown = false;
+    }
+}
